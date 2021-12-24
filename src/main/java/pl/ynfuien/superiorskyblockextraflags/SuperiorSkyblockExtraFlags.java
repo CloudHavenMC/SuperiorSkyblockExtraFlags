79efc372a721cc5828d5c12e906e698fd00c02bf
Ynfuien <unconfigured@null.spigotmc.org>
2021-12-24 23:56:08 +0100

    Added check for missing SS2 plugin

--- src/main/java/pl/ynfuien/superiorskyblockextraflags/SuperiorSkyblockExtraFlags.java
+++ src/main/java/pl/ynfuien/superiorskyblockextraflags/SuperiorSkyblockExtraFlags.java
@@ -1,5 +1,7 @@
 package pl.ynfuien.superiorskyblockextraflags;
 
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import pl.ynfuien.superiorskyblockextraflags.listeners.SuperiorInitializeListener;
 import pl.ynfuien.superiorskyblockextraflags.utils.Util;
@@ -10,12 +12,20 @@ public final class SuperiorSkyblockExtraFlags extends JavaPlugin {
     public void onEnable() {
         Util.setInstance(this);
 
+        PluginManager pm = getServer().getPluginManager();
+
+        if (pm.getPlugin("SuperiorSkyblock2") == null) {
+            Util.log(Util.formatColors("&cThere is no SuperiorSkyblock2 on your server! Plugin can't work without it."));
+            pm.disablePlugin(this);
+            return;
+        }
+
         saveDefaultConfig();
         Util.setIslandProtectedMessage(getConfig().getString("island-protected-message"));
         Util.setHexPrefix(getConfig().getString("hex-prefix"));
 
         // Register island flags and privileges
-        getServer().getPluginManager().registerEvents(new SuperiorInitializeListener(this), this);
+        pm.registerEvents(new SuperiorInitializeListener(this), this);
 
         Util.log("Plugin successfully enabled!");
     }
