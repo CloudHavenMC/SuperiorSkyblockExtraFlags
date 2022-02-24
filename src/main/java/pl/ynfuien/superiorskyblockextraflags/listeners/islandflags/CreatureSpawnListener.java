d71f024db272bf4dc323a9abe6211e7e4171ac3f
Ynfuien <unconfigured@null.spigotmc.org>
2022-02-24 21:09:48 +0100

    Little code corrections

--- src/main/java/pl/ynfuien/superiorskyblockextraflags/listeners/islandflags/CreatureSpawnListener.java
+++ src/main/java/pl/ynfuien/superiorskyblockextraflags/listeners/islandflags/CreatureSpawnListener.java
@@ -11,41 +11,49 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 
 public class CreatureSpawnListener implements Listener {
+    // Island flags that this event handles:
+    // - NATURAL_MONSTER_SPAWN_NORMAL
+    // - NATURAL_MONSTER_SPAWN_NETHER
+    // - NATURAL_MONSTER_SPAWN_THE_END
 
     @EventHandler (ignoreCancelled = true)
     public void onCreatureSpawn(CreatureSpawnEvent e) {
+        // Get entity
         LivingEntity entity = e.getEntity();
 
+        // Return if entity isn't monster
         if (!isMonster(entity)) return;
 
+        // Return if spawn reason isn't natural spawn
         if (!e.getSpawnReason().equals(CreatureSpawnEvent.SpawnReason.NATURAL)) return;
 
+        // Get entity location
         Location location = entity.getLocation();
-
+        // Get island at location
         Island island = SuperiorSkyblockAPI.getIslandAt(location);
+        // Return if no island is at entity location
         if (island == null) return;
 
-        if (!e.getSpawnReason().equals(CreatureSpawnEvent.SpawnReason.NATURAL)) return;
+        // Get world environment
         World.Environment environment = location.getWorld().getEnvironment();
 
+        // Set flag
+        String flag = "NATURAL_MONSTER_SPAWN_"+environment.name();
+        // Change flag if world's environment is CUSTOM
+        if (environment.equals(World.Environment.CUSTOM)) flag = "NATURAL_MONSTER_SPAWN_NORMAL";
 
-        if (!environment.equals(World.Environment.NORMAL) && !environment.equals(World.Environment.CUSTOM)) {
-            if (!island.hasSettingsEnabled(IslandFlag.getByName("NATURAL_MONSTER_SPAWN_"+environment.name()))) {
-                e.setCancelled(true);
-            }
-            return;
-        }
-
-        if (!island.hasSettingsEnabled(IslandFlag.getByName("NATURAL_MONSTER_SPAWN_NORMAL"))) {
+        // Cancel event if island has flag disabled
+        if (!island.hasSettingsEnabled(IslandFlag.getByName(flag))) {
             e.setCancelled(true);
-            return;
         }
     }
 
+    // Gets whether entity is a monster or not
     public static boolean isMonster(Entity e) {
         if (e instanceof Monster) return true;
         if (e instanceof Slime) return true;
         if (e instanceof Flying) return true;
+
         return false;
     }
 }
